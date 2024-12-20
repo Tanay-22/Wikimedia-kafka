ae14c0b4f24411f89c41ec2b097950d24c430d68
Tanay Pandey <tanay22pandey@gmail.com>
2024-12-20 21:50:37 +0530

    created kafka producer for wikimedia

--- src/main/java/com/tanay/producer/WikimediaChangeHandler.java
+++ src/main/java/com/tanay/producer/WikimediaChangeHandler.java
@@ -1,4 +1,54 @@
 package com.tanay.producer;
 
-public class WikimediaChangeHandler {
+import com.launchdarkly.eventsource.MessageEvent;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangeHandler implements BackgroundEventHandler
+{
+    KafkaProducer<String, String> kafkaProduce;
+    String topic;
+    private final Logger log = LoggerFactory.getLogger(WikimediaChangeHandler.class.getName());
+
+
+    public WikimediaChangeHandler(KafkaProducer<String, String> kafkaProduce, String topic)
+    {
+        this.kafkaProduce = kafkaProduce;
+        this.topic = topic;
+    }
+
+    @Override
+    public void onOpen()
+    {
+        // nothing here
+    }
+
+    @Override
+    public void onClosed()
+    {
+        kafkaProduce.close();
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent)
+    {
+        log.info(messageEvent.getData());
+        // asynchronous
+        kafkaProduce.send(new ProducerRecord<>(topic, messageEvent.getData()));
+    }
+
+    @Override
+    public void onComment(String s)
+    {
+        // nothing here
+    }
+
+    @Override
+    public void onError(Throwable throwable)
+    {
+        log.error("Error in Stream Reading", throwable);
+    }
 }
--- src/main/java/com/tanay/producer/WikimediaChangesProducer.java
+++ src/main/java/com/tanay/producer/WikimediaChangesProducer.java
@@ -1,5 +1,50 @@
 package com.tanay.producer;
 
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.StreamException;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class WikimediaChangesProducer
 {
+    public static void main(String[] args) throws InterruptedException, StreamException
+    {
+        String bootstrapServer = "localhost:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // set safe producer configs (Kafka <= 2.8)
+//        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+//        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+//        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
+        //create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(url));
+        BackgroundEventSource backgroundEventSource =
+                new BackgroundEventSource.Builder(eventHandler, eventSourceBuilder)
+                        .build();
+
+        // start the producer in another thread
+        backgroundEventSource.start();
+
+        // we produce for 10 minutes and block the program until then
+        TimeUnit.MINUTES.sleep(10);
+    }
 }
